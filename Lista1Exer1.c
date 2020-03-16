ab2747f50086665272bdc157592f8404bd9a8061
pedrohfd <62259990+pedrohfd@users.noreply.github.com>
2020-03-16 15:47:44 -0300

    Dicotomia

---
+++ Lista1Exer1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void main(){
+	
+	float i=0,a,b,m,fa,fm,fb;
+	
+	printf("Digite o Valor de A - ");
+	scanf("%f",&a);
+	printf("Digite o Valor de B - ");
+	scanf("%f",&b);
+	
+	while(fm>=0.05){
+		m = (a+b)/2;
+		fa = (pow(a,3)+a-4);
+		fb = (pow(b,3)+b-4);
+		fm = (pow(m,3)+m-4);
+		
+		
+	}
+}
